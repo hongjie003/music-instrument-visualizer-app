a6d907c66d74e266874a0765416b8f9efed9f768
hongjie <hongjieli003@gmail.com>
2022-05-09 15:01:27 -0700

    Add more songs

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -9,5 +9,7 @@ CREATE TABLE songs (
 
 INSERT INTO songs (id, song_title, notes, artist, genre_title, genre_color) 
 VALUES 
-	(1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 'Blue Claw Philharmonic', 'Classical / Dubstep', '#e8b3e1'),
-	(2, 'Sweet Child O Mine', 'C4 C5 G4 F4 F5 G4 E5 G4 C4 C5 G4 F4 F5 G4 E5 G4 D4 C5 G4 F4 F5 G4 E5 G4 D4 C5 G4 F4 F5 G4 E5 G4 F4 C5 G4 F4 F5 G4 E5 G4 F4 C5 G4 F4 F5 G4 E5 G4 C4 C5 G4 F4 F5 G4 E5 G4 C4 C5 G4 F4 F5 G4 E5 G4', "Guns N' Roses", 'Rock', '#edadaf');
+	(1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 'Ludwig van Beethoven', 'Classical / Dubstep', '#e8b3e1'),
+	(2, 'Sweet Child O Mine', 'C4 C5 G4 F4 F5 G4 E5 G4 C4 C5 G4 F4 F5 G4 E5 G4 D4 C5 G4 F4 F5 G4 E5 G4 D4 C5 G4 F4 F5 G4 E5 G4 F4 C5 G4 F4 F5 G4 E5 G4 F4 C5 G4 F4 F5 G4 E5 G4 C4 C5 G4 F4 F5 G4 E5 G4 C4 C5 G4 F4 F5 G4 E5 G4', "Guns N' Roses", 'Rock', '#edadaf'),
+	(3, 'Sunshine of Your Love (Remix) ', 'D5 D5 C5 D5 A4 A4 G#4 G#4 G4 G4 D4 F4 D4 D5 D5 C5 D5 A4 A4 G#4 G#4 G4 G4 D4 F4 G4 D5 D5 C5 D5 A4 A4 G# G#4 G4 G4 D5 F5 D5 D5 D5 C5 D5 A4 A4 G#4 G#4 G4 G4 D5 F5 F5 D5', 'Cream', 'Pop / Rock / Blues', '#b1c8de'),
+	(4, 'Come As You Are (Remix)', 'D4 D4 D#4 E4 E4 G4 E4 G4 E4 E4 D#4 D4 A4 D4 D4 A4 D4 D4 D#4 E4 E4 G4 E4 G4 E4 E4 D#4 D4 A4 D4 D4 A4 D4 D4 D#4 E4 E4 G4 E4 G4 E4 E4 D#4 D4 A4 D4 D4 A4 D4 D#4', 'Nirvana', 'Pop / Rock', '#edadaf');
